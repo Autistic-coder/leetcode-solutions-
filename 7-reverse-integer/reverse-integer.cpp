30c33f308215e61f7dc5b6fd4b6c2cb54a28db73
tf141 coder <vaibhavbedi07@gmail.com>
2026-09-10 09:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.42%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,30 @@
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check overflow before multiplying by 10
+            if (reversed > INT_MAX / 10 ||
+                (reversed == INT_MAX / 10 && digit > 7)) {
+                return 0;
+            }
+
+            // Check underflow before multiplying by 10
+            if (reversed < INT_MIN / 10 ||
+                (reversed == INT_MIN / 10 && digit < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+};
